7c738bf001b2dcbaafdc136b8308b08bb3a6bfa4
dlywlotus <dylanlowywgit@gmail.com>
2026-04-04 11:02:05 +0800

    First commit

---
+++ backend/src/test/java/com/dlywlotus/echo_backend/EchoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlywlotus.echo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
