d6f586861c90718a2ebe84085e479a7891926c46
dlywlotus <dylanlowywgit@gmail.com>
2026-04-13 22:57:50 +0800

    Style home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,28 @@
-const HomePage = () => {
-  return <div>Home</div>
-}
+import { Input } from "@/components/ui/input"
+import { BotMessageSquare } from "lucide-react"
 
-export default HomePage
+import CustomCard from "@/components/custom/CustomCard"
+import { Button } from "@/components/ui/button"
+
+export default function HomePage() {
+  return (
+    <CustomCard>
+
+      <div className="mx-auto bg-primary/10 w-24 h-24 rounded-xl flex items-center justify-center">
+        <BotMessageSquare className="text-primary w-12 h-12" />
+      </div>
+      <div className="text-base">
+        <span className="text-primary">Echo,</span> annonymous real time chats
+      </div>
+      <div className="w-full">
+        <label className="self-start">
+          Display Name
+        </label>
+        <Input className="mt-2"
+          placeholder="e.g. dlywlotus"
+        />
+      </div>
+      <Button className="w-full" size={"lg"}>Start chatting</Button>
+    </CustomCard>
+  )
+}
--- frontend/src/pages/RootLayout.tsx
+++ frontend/src/pages/RootLayout.tsx
@@ -1,12 +1,9 @@
-import { Card } from "@/components/ui/card"
 import { Outlet } from "react-router"
 
 const RootLayout = () => {
   return (
-    <div className="flex min-w-svh flex-col items-center justify-center">
-      <Card className="w-full max-w-lg">
-        <Outlet />
-      </Card>
+    <div className="flex flex-col items-center justify-center h-svh">
+      <Outlet />
     </div>
   )
 }
